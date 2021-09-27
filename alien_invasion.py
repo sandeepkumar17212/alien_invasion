aa70d3276557b0453e90b64d958efdf04cb4a23f
sandeepkumar17212 <sandeepkumar17212@gmail.com>
2021-09-27 18:10:03 +1000

    build the front window for the game, which can listen the event by keybord  and have a close function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,38 @@
 import sys
 import pygame
+from pygame.constants import RESIZABLE
 
 
 
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 700))
-    pygame.display.set_caption("Alien Invasion")
+
+    ##sys.flags gives us the ability to resize the windows. 
+    ##sys.flags = RESIZABLE
+    sys.screen = pygame.display.set_mode((1200, 600))
+    pygame.display.set_caption("Alien Invasion - by Sandeep Kumar")
+
+    #set veriable for background color.
+    bg_color = (230,230,230)
+
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        #this will fill the screen with given
+        #color on every while loop.
+        sys.screen.fill(bg_color)
+
         # Make the most recently drawn screen visible.
+        # it hide the old screen and draw the new one.
         pygame.display.flip()
+
+
+
+
 run_game()
+
